82663f32d718fc5f8c93b7380f95647cc655bc64
anicperica <anicperica18@gmail.com>
2025-10-09 17:11:24 +0200

    Remove old frontend submodule reference

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+
+
+  return (
+     <Routes>
+     <Route path="/" element={<h1>Početna</h1>} />
+      </Routes>
+      
+  )
+}
+
+export default App
