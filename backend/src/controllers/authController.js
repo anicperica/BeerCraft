9358b60e15008d95dafc99709df4fedece44a700
anicperica <anicperica18@gmail.com>
2026-02-15 15:51:11 +0100

    Fix URL for deployment

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -19,9 +19,9 @@ export const registerUser = async (req, res) => {
     const token = generateToken(user._id);
 
     res.cookie("jwt", token, {
-      httpOnly: false,
-      secure: false,
-      sameSite: "lax",
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
       path: "/",
     });
@@ -44,9 +44,9 @@ export const loginUser = async (req, res) => {
     if (user && (await user.matchPassword(password))) {
       const token = generateToken(user._id);
       res.cookie("jwt", token, {
-        httpOnly: false,
-        secure: false,
-        sameSite: "lax",
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000,
         path: "/",
       });
