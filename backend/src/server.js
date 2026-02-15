10e1ba604f10e350831a3f18d1ff18374c5143f3
anicperica <anicperica18@gmail.com>
2026-02-15 15:55:27 +0100

    Fix error

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,45 @@
-c
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import beerRoutes from "./routes/beerroutes.js"
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import breweryRoutes from "./routes/breweryRoutes.js"
+import beerAdminRoutes from "./routes/beerAdminRoutes.js"
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser())
+app.use(
+  cors({
+    origin: "https://beer-craft.vercel.app", 
+    credentials: true, 
+  })
+);
+//http://localhost:5173
+//a
+app.use("/api/auth", authRoutes);
+app.use("/api/beers", beerRoutes);
+app.use("/api/brewery", breweryRoutes);
+app.use("/api/admin", beerAdminRoutes);
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(
+        `Server running  on port ${PORT}`
+      );
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
